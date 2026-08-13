bb61d1b7609821f1d9e94805318286b5f3c370a6
ElnoraKamolova <elnora6520@gmail.com>
2026-08-13 14:50:52 +0500

    Добавила кнопку

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+    </div>
+  );
+};
 
-export default App
+export default App;
